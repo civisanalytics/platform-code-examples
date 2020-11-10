66aba4c114837de94dcea2d443a39659f095081c
Teddy Morris-Knower <ted107mk@gmail.com>
2020-11-10 11:07:55 -0500

    Update documentation for container script

--- examples/parsons_civis_twilio_example.py
+++ examples/parsons_civis_twilio_example.py
@@ -1,44 +1,75 @@
 # This script is an example of using the parsons library to import data from
 # Twilio using the parsons Twilio connector, and then upload the resulting
 # table to Platform using the parsons Civis connector.
-from parsons.civis.civisclient import CivisClient
-from parsons.twilio.twilio import Twilio
+# Below are instructions to be able to customize and run this file in Platform.
 
-# Step 1: Open a new Python script in Platform, and paste this entire file
-#         into the script body.
-#         TODO: this would require adding parsons to datascience-python
+# Step 1: Copy this file to a github repository that is connected to Platform.
+#         For docs on how to connect Platform to Github, follow the steps in
+#         the first two paragraphs under "Connecting Platform to Github/Bitbucket":
+#         https://civis.zendesk.com/hc/en-us/articles/115003734992-Version-Control
 
-# Step 2: Set CIVIS_DATABASE and CIVIS_TABLE variables.
+
+# Step 2: Create a new container script in Platform with the following fields:
+#         GITHUB REPOSITORY URL: The github repository from Step 1
+#         GITHUB REPOSITORY REFERENCE: The branch name that contains the copy of this file.
+#         COMMAND: python /app/<path to the copy of this file>
+#         DOCKER IMAGE NAME: movementcooperative/parsons
+#         DOCKER IMAGE TAG: v0.16.0
+#         Docs: https://civis.zendesk.com/hc/en-us/articles/218200643-Container-Scripts
+
+
+# Step 3: Set CIVIS_DATABASE and CIVIS_TABLE global variables below
 #         CIVIS_TABLE should be in "schema.table" form
 #         See list of databases: platform.civisanalytics.com/spa/remote_hosts
 
-# Step 3: Set environment variables necessary for the clients:
-#         CIVIS_API_KEY: Civis API key
-#         TWILIO_ACCOUNT_SID: Twilio Account SID
-#         TWILIO_AUTH_TOKEN: Twilio Auth Token
+CIVIS_DATABASE = 32
+CIVIS_TABLE = "twilio_test.account_usage"
+
+# Step 4: Create a platform custom credential with the following fields:
+#         Name: Twilio Account
+#         Credential Type: Custom
+#         Username: Twilio Account SID
+#         Password: Twilio Auth Token
+#
+#         To create platform credentials: platform.civisanalytics.com/spa/credentials/new
 #         Note: both twilio variables can be found at www.twilio.com/console
+
+# Step 5: Run the container script!
 
 # For complete documentation on parsons twilio & civis connectors, see:
 # Civis: github.com/move-coop/parsons/blob/master/parsons/civis/civisclient.py
 # Twilio: github.com/move-coop/parsons/blob/master/parsons/twilio/twilio.py
 
-CIVIS_DATABASE = 99
-CIVIS_TABLE = "twilio_test.account_usage"
+from parsons.civis.civisclient import CivisClient
+from parsons.twilio.twilio import Twilio
+import os
 
-civis_client = CivisClient(db=CIVIS_DATABASE)
-twilio_client = Twilio()
+class CivisTwilioConnector:
 
-
-def twilio_to_civis():
-    table = twilio_client.get_account_usage()
-    civis_client.table_import(table, CIVIS_TABLE, existing_table_rows='drop')
-
-
-def read_from_civis():
-    query = f"SELECT * from {CIVIS_TABLE}"
-    table = civis_client.query(query)
-    print(table)
+    def __init__(self):
+        self.validate_params()
+        self.civis_client = CivisClient(db=CIVIS_DATABASE)
+        self.twilio_client = Twilio(
+            account_sid=os.environ.get('TWILIO_ACCOUNT_USERNAME'),
+            auth_token=os.environ.get('TWILIO_ACCOUNT_PASSWORD')
+            )
 
 
-twilio_to_civis()
-read_from_civis()
+    def twilio_to_civis(self):
+        table = self.twilio_client.get_account_usage()
+        self.civis_client.table_import(table, CIVIS_TABLE, existing_table_rows='drop')
+
+
+    def read_from_civis(self):
+        query = f"SELECT * from {CIVIS_TABLE}"
+        table = self.civis_client.query(query)
+        print(table)
+
+    def validate_params(self):
+        pass
+        # TODO: validate params
+
+if __name__ == "__main__":
+    connector = CivisTwilioConnector()
+    connector.twilio_to_civis()
+    connector.read_from_civis()
