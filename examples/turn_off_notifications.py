1215c059b6d682b1c492f1ce5e4487f3f180e465
Gabe Mendoza <gmendoza@civisanalytics.com>
2022-12-01 14:28:47 -0600

    manual pr review updates

--- examples/turn_off_notifications.py
+++ examples/turn_off_notifications.py
@@ -16,47 +16,30 @@ def bulk_fetch(api_method, user_id):
 
   return jobs
 
-# LM: I'd recommend moving these statements to the top of the file and adding a readme of how to change the success & failure_off variables.
-# This looks like it expects env variables to change those params; that's clean for turning this into a reusable script (we could provide one with those vars for CS, make user id a var too?),
-# but I'm guessing that it'd be easier for most client users simply change a "true" string to a "false" string.
-# Oh, now I see that there are actually variables at the top - cool! Those probably won't transfer to Zendesk though.
 client = civis.APIClient()
 user = client.users.list_me()
 user_id = f'{user.id}'
 
 success_off = os.getenv('success_off', 'false') == 'true'
 failure_off = os.getenv('failure_off', 'false') == 'true'
+handle_workflows = os.getenv('handle_workflows', 'false') == 'true'
 
-# LM: Oh, interesting! We expect people to use this script to turn off notifications, but I wonder if anyone will want to use it in reverse.
-# Might discover this later, but now I'm also wondering if this script could end up turning on notifications for some jobs,
-# if say, only success_off is selected, will failure emails then be enabled for all jobs, even if they had been selectively disabled previously?
-# Preventing that situation could be a good reason to say that this script only supports turning off notifications.
 if success_off == False and failure_off == False:
     raise Exception("Either success_off or failure_off, or both, must be selected")
 
+notifications = {}
 turning_off_notifs_str = None
 if success_off == True and failure_off == True:
     turning_off_notifs_str = "success and failure"
+    notifications.update({'success_on': not success_off})
+    notifications.update({'failure_on': not failure_off})
 if success_off == True and failure_off == False:
     turning_off_notifs_str = "success"
+    notifications.update({'success_on': not success_off})
 else:
     turning_off_notifs_str = "failure"
+    notifications.update({'failure_on': not success_off})
 
-# LM: Hm, so I think this will have the effect which I mentioned previously: we could end up affecting notification which we're not trying to change.
-# Preserving the original setting for one or the other might make the logic more complex though, since this is a hash.
-# It looks like success_email_addresses, etc are also part of that same hash. Can we simply leave out items we don't wish to modify?
-# Ideally, platform would merge the items in the patch request with the existing settings for the unspecified items; I'm not sure if that's how it works or not though.
-notifications = {'success_on': not success_off, 'failure_on': not failure_off}
-
-
-# JOBS
-# LM: Could probably say "scripts" instead of "jobs" in this log here I think.
-# Hm, I wonder if we could use the list scripts endpoint instead since it looks like those are the only types of job which this particular section handles.
-# From the API docs, I'm not positive if that would return custom scripts too though.
-# If we can use it, that would get us away from the possibility of users having jobs of other types though.
-# (e.g. I expect that some Civis users will still have Media Optimizer jobs; don't think we need to/should modify deprecated job types, but they could muck up the script)
-# Might be better to explicitly modify just scripts and imports, rather than try to collect an exhaustive list of different job types to put in client visible code?
-# Oh wait, I didn't see the error handling on L92. This might be fine after all, since additional job types won't crash the script.
 print(f'Turning off {turning_off_notifs_str} notifications for scripts owned by user id {user_id}')
 jobs = bulk_fetch(client.jobs.list, user_id)
 
@@ -91,6 +74,7 @@ for job in jobs:
   except Exception as err:
       failed_to_update.append(f" ({job.type} ID {job.id}) - Unexpected error {err}")
 
+
 # IMPORTS
 print(f'Turning off {turning_off_notifs_str} notifications for imports owned by user id {user_id}')
 imports = bulk_fetch(client.imports.list, user_id)
@@ -100,16 +84,17 @@ for import_job in imports:
   except Exception as err:
     failed_to_update.append(f" (Import ID {import_job.id}) - Unexpected error {err}")
 
-# LM: I wonder if it would be worthwhile to make processing of jobs/workflows configurable via a variable. e.g. HANDLE_WORKFLOWS = true
-# Users could just comment out the section they don't want, but it might be a bit friendlier to include that as a top level option.
-# WORKFLOWS
-print(f'Turning off {turning_off_notifs_str} notifications for workflows owned by user id {user_id}')
-workflows = bulk_fetch(client.workflows.list, user_id)
-for workflow in workflows:
-  try:
-    client.workflows.patch(workflow.id, notifications=notifications)
-  except Exception as err:
-    failed_to_update.append(f" (Workflow ID {workflow.id}) - Unexpected error {err}")
+
+if handle_workflows == True:
+  # WORKFLOWS
+  print(f'Turning off {turning_off_notifs_str} notifications for workflows owned by user id {user_id}')
+  workflows = bulk_fetch(client.workflows.list, user_id)
+  for workflow in workflows:
+    try:
+      client.workflows.patch(workflow.id, notifications=notifications)
+    except Exception as err:
+      failed_to_update.append(f" (Workflow ID {workflow.id}) - Unexpected error {err}")
+
 
 # Print Failed Jobs
 print(f"Failed to turn off notifications for {len(failed_to_update)} jobs")
