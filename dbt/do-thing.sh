53a429031819f309217bcf4329dbcbdedfd00e67
Gabe Mendoza <gmendoza@civisanalytics.com>
2025-07-28 20:34:25 +0000

    smol changes

---
+++ dbt/do-thing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# dbt test --select dbt_project.yml --project-dir /workspace/platform-code-examples/dbt --profiles-dir /workspace/platform-code-examples/dbt --target redshift_adapter
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+# sudo apt-get install build-essential procps curl git
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# awk -F: '{printf "%s:%s\n",$1,$3}' /etc/passwd
+curl https://mise.run | sh
